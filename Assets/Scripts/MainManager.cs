0697b1475cf6866777bf5987ff03266b4f79d12a
JoeWarren95 <jwarren1495@gmail.com>
2022-08-27 20:18:28 -0400

    Added regions for my code, and notes as to next steps
    Buttons transitioning between scenes, and text box added into scene hierarchy. No functionality in these gameobjects yet however.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
+    //make MainManager into a Singleton
+    public static MainManager Instance;
+
+    #region My Variables
+    public string PlayerName;
+    public int highScore;
+    public GameObject HighScoreText;
+
+    private bool isNewHighScore;
+
+    #endregion
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
@@ -18,7 +30,56 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
-    
+    #region My code additions
+    //add this to check there are no other main managers in the scene
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        HighScoreText.gameObject.SetActive(false);
+    }
+
+    //I want to save the player's name, and their highscore if they got it
+    [System.Serializable]
+    class SaveData
+    {
+        //this is the data I want to save
+        public string PlayerName;
+        public int highScore;
+    }
+
+    public void SaveName()
+    {
+        //save the players name between scenes
+        SaveData data = new SaveData();
+        data.PlayerName = PlayerName;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            PlayerName = data.PlayerName;
+        }
+    }
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -53,13 +114,24 @@ public class MainManager : MonoBehaviour
                 Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
             }
         }
-        else if (m_GameOver)
+
+        #region Adding check here for new high score
+        //we need to check if there was a new high score first
+        //if there was a new high score, set different text active
+        //also set text box active for player to enter their name
+        else if (isNewHighScore && m_GameOver)
+        {
+            HighScoreText.gameObject.SetActive(true);
+        }
+       
+        else if (m_GameOver && !isNewHighScore)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+        #endregion
     }
 
     void AddPoint(int point)
@@ -73,4 +145,16 @@ public class MainManager : MonoBehaviour
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+
+    #region Button Code
+    public void StartNew()
+    {
+
+    }
+
+    public void Exit()
+    {
+        
+    }
+    #endregion
 }
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public MainManager mainManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //this function will be called whenever a new game is started
+    public void StartNew()
+    {
+        //load scene 1 in build settings which is our game
+        SceneManager.LoadScene(1);
+    }
+
+    //this function is called whenever we want to exit the game
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        //this line will take us back to the main menu
+        SceneManager.LoadScene(0);
+#else
+        Application.Quit();
+        SceneManager.LoadScene(0);
+#endif
+
+    }
+}
